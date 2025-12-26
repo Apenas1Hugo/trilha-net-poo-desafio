d3a789c172a0d7eec960bbb358ecb851f73ba919
Apenas1Hugo <hugorodriguesm27@gmail.com>
2025-12-26 09:29:26 -0300

    implementando Nokia
    classe Nokia arrumada.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia(
+            numero: "11999999999",
+            modelo: "Nokia 3310",
+            imei: "123456789012345",
+            memoria: 64);
+
+  nokia.InstalarAplicativo("WhatsApp");
