037df590228c83ef094f628f23c58ced6a71ba38
Apenas1Hugo <hugorodriguesm27@gmail.com>
2025-12-26 09:34:24 -0300

    Iphone e testes
    classe iphone arrumada e testes feitos.

--- Program.cs
+++ Program.cs
@@ -7,4 +7,14 @@ Nokia nokia = new Nokia(
             imei: "123456789012345",
             memoria: 64);
 
+Iphone iphone = new Iphone(
+            numero: "11999999999",
+            modelo: "Iphone 14",
+            imei: "123456789345",
+            memoria: 32);
+  
+  
+  
+  
   nokia.InstalarAplicativo("WhatsApp");
+  iphone.InstalarAplicativo("clash Royale");
